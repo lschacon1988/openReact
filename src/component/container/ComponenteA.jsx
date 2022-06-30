c732c7595b4c217c471b1068d518744855e26750
lschacon1988 <lschacon1988@gmail.com>
2022-06-30 06:46:17 -0500

    actulizado

--- src/component/container/ComponenteA.jsx
+++ src/component/container/ComponenteA.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from 'react';
-import { Contacto } from '../../model/Contacto';
-import ComponenteB from '../pure/ComponenteB';
-
-
+import React, { useState } from "react";
+import { Contacto } from "../../model/Contacto";
+import ComponenteB from "../pure/ComponenteB";
 
 const ComponenteA = () => {
-    const contacto = new Contacto('luis', 'Chacon', 'lschacon61@gmail.com', false)
-    const [state, setState]= useState(contacto.conectado)
+  const contacto = new Contacto(
+    "luis",
+    "Chacon",
+    "lschacon61@gmail.com",
+    false
+  );
+  const [state, setState] = useState(contacto.conectado);
   const changeState = (e) => {
     // e.preventDefault()
-    setState(!state)
+    setState(!state);
   };
-    return (
+  return (
     <div>
-        <ComponenteB contacto={contacto} changeState={changeState} state={state} />
+      <ComponenteB
+        contacto={contacto}
+        changeState={changeState}
+        state={state}
+      />
     </div>
-    );
-}
-
-
-
+  );
+};
 
 export default ComponenteA;
